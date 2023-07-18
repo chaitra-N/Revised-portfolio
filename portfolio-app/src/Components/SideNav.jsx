6a2955b8748a11f5daf71197e3f764b190172512
Chai <developer.chaitra@gmail.com>
2023-07-19 09:59:33 +1200

    SideNav

--- portfolio-app/src/Components/SideNav.jsx
+++ portfolio-app/src/Components/SideNav.jsx
@@ -1,10 +1,67 @@
 import React from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import {
+  AiOutlineHome,
+  AiOutlineMenu,
+  AiOutlineUser,
+  AiOutlineFilePdf,
+  AiOutlineMail,
+} from "react-icons/ai";
+import { GrProjects } from "react-icons/gr";
+import { useState } from "react";
 
 const SideNav = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
     <div>
-      <AiOutlineMenu />
+      <AiOutlineMenu
+        onClick={handleNav}
+        className='absolute top-4 right-4 z-[99] md:hidden'
+      />
+      {nav ? (
+        <div className='fixed w-full h-screen bg-violet-300 flex flex-col justify-center items-center z-20'>
+          <a
+            href='#main'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+          >
+            <AiOutlineHome size={20} />
+            <span className='pl-4'>Home</span>
+          </a>
+          <a
+            href='#main'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+          >
+            <AiOutlineUser size={20} />
+            <span className='pl-4'>About</span>
+          </a>
+          <a
+            href='#main'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+          >
+            <GrProjects size={20} />
+            <span className='pl-4'>Projects</span>
+          </a>
+          <a
+            href='#main'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+          >
+            <AiOutlineFilePdf size={20} />
+            <span className='pl-4'>Resume</span>
+          </a>
+          <a
+            href='#main'
+            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
+          >
+            <AiOutlineMail size={20} />
+            <span className='pl-4'>Contact</span>
+          </a>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
